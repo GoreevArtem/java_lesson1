4bc559ae75c9af104a323d9baf6f6ae7a0b7d232
Goreev Artem <goreevartoum@gmail.com>
2021-03-31 00:51:24 +0300

    Added logic to the app

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,8 +1,16 @@
 package com.company;
 
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        Scanner input = new Scanner(System.in);
+	    System.out.println("Do you want pizza?");
+	    String text = input.nextLine();
+	    if (text.toUpperCase().equals("YEs"))
+            System.out.println("I'm gonna go make your pizza");
+        else if (text.toUpperCase().equals("NO"))
+            System.out.println("Good bye");
     }
 }
