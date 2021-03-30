3d38b739e70bc6347ac07acf34f6e2f77dcc86e5
Goreev Artem <goreevartoum@gmail.com>
2021-03-31 00:56:08 +0300

    release

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -6,11 +6,13 @@ public class Main {
 
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
-	    System.out.println("Do you want pizza?");
+	    System.out.println("Do you want pizza? (yes/no)");
 	    String text = input.nextLine();
 	    if (text.toUpperCase().equals("YES"))
             System.out.println("I'm gonna go make your pizza");
         else if (text.toUpperCase().equals("NO"))
             System.out.println("Good bye");
+        else
+            System.out.println("You entered the data incorrectly");
     }
 }
