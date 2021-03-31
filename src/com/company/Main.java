6f5f9e477914879fcb7a69f2f8ec4195d2b46499
Goreev Artem <goreevartoum@gmail.com>
2021-03-31 19:15:44 +0300

    ru

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -6,12 +6,12 @@ public class Main {
 
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
-	    System.out.println("Do you want pizza? (yes/no)");
-	    String text = input.nextLine();
-	    if (text.toUpperCase().equals("YES"))
-            System.out.println("I'm gonna go make your pizza");
-        else if (text.toUpperCase().equals("NO"))
-            System.out.println("Good bye");
+        System.out.println(" Ты хочешь покушац пиццу?");
+        String text = input.nextLine();
+        if (text.toUpperCase().equals("ДА"))
+            System.out.println("Пицца приготовлена, кушац подано!");
+        else if (text.toUpperCase().equals("НЕТ"))
+            System.out.println("Пока!");
         else
             System.out.println("You entered the data incorrectly");
     }
